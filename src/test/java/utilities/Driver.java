89a36c4a3f4258b1bd8b0b9c617f930dbcb8cd3c
HakanKara75 <hakanbatirhan@gmail.com>
2023-09-08 15:24:56 +0300

    v

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,7 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -37,6 +38,11 @@ public class Driver {
                 case "edge":
                     WebDriverManager.edgedriver().setup();
                     driver = new EdgeDriver();
+                    break;
+                case "chrome-headless":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+                    //bu secenekte chrome acilmadan test kosulur
                     break;
                 default:
                     WebDriverManager.chromedriver().setup();
